044fea9f5f25d2bf3c566766e2aee5a1a2801a1a
Gaurab Chhetri <contact@gaurabchhetri.com.np>
2025-01-17 20:47:56 -0600

    [fix]: braking changes

--- init.lua
+++ init.lua
@@ -7,7 +7,6 @@ require("tabnine").setup({
   dismiss_keymap = "<C-]>",
   debounce_ms = 800,
   suggestion_color = { gui = "#808080", cterm = 244 },
-  exclude_filetypes = { "TelescopePrompt" },
   log_file_path = nil, -- absolute path to Tabnine log file
 })
 
--- lua/config/keymaps.lua
+++
@@ -1,9 +0,0 @@
--- Define which-key mappings for the toggleterm plugin
-require("which-key").register({
-  ["<leader>t"] = {
-    name = "Terminal",
-    l = { "<cmd>ToggleTerm direction=vertical size=40<cr>", "Open vertical terminal" },
-    b = { "<cmd>ToggleTerm direction=horizontal size=20<cr>", "Open horizontal terminal" },
-    f = { "<cmd>ToggleTerm direction=float<cr>", "Open floating terminal" },
-  },
-})
